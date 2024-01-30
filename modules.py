1d1a426f28f9ad78bad8f7143dc4f4c633c006d0
Lunar <lvaserveremail@gmail.com>
2024-01-30 11:34:12 -0700

    Update and rename GPS.py to modules.py

---
+++ modules.py
@@ -0,0 +1,13 @@
+## DEBUG, real script will have much much more
+callout = "Current module: "
+module = "None"
+if currentmodule = 0
+  print(callout + "None")
+  module = "None"
+elif currentmodule = 1
+  print(callout + "Sound")
+  module = "Sound"
+elif currentmodue = 2
+  print(callout + "Ultrasonic Distance")
+  module = "Ultrasonic Distance"
+else
