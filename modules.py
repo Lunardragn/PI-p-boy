3ea34e94bf75594929f966fe33333e016d95a022
Lunar <lvaserveremail@gmail.com>
2024-01-30 12:44:34 -0700

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,15 +1,20 @@
 ## DEBUG, real script will have much much more
 callout = "Current module: "
 module = "None"
-if currentmodule = 0
-  print(callout + "None")
-  module = "None"
-elif currentmodule = 1
-  print(callout + "Sound")
-  module = "Sound"
-elif currentmodule = 2
-  print(callout + "Ultrasonic Distance")
-  module = "Ultrasonic Distance"
-elif currentmodule =3
-  print(callout + "GPS")
-  module = "GPS"
+currentmodule = int(input("Select module type:"))
+if currentmodule == 0:
+	print(callout + "None")
+	module = "None"
+elif currentmodule == 1:
+	print(callout + "Sound")
+	module = "Sound"
+elif currentmodule == 2:
+	print(callout + "Ultrasonic Distance")
+	module = "Ultrasonic Distance"
+elif currentmodule == 3:
+	print(callout + "GPS")
+	module = "GPS"
+else:
+	print(callout + "None")
+	module = "None"
+print(module)
